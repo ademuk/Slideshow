536f59a9c4be25f59f82cc6168c595c0683b9671
Adem Gaygusuz <adem@ardweb.co.uk>
2014-10-05 23:22:36 +0100

    Added default window size

--- Slideshow/AppDelegate.m
+++ Slideshow/AppDelegate.m
@@ -21,6 +21,13 @@
     [self.window.contentView addSubview:self.viewController.view];
     [self.viewController.view setFrameSize:self.window.frame.size];
     
+    NSRect screenSize = [[NSScreen mainScreen] frame];
+    
+    CGFloat percent = 0.6;
+    CGFloat offset = (1.0 - percent) / 2.0;
+    
+    [self.window setFrame:NSMakeRect(screenSize.size.width * offset, screenSize.size.height * offset, screenSize.size.width * percent, screenSize.size.height * percent) display:YES];
+    
     NSResponder *aNextResponder = [self.window nextResponder];
     
     [self.window setNextResponder:self.viewController];
